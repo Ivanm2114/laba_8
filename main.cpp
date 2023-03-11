0e19df2f4814817af19af0155a1c606fed4b57b3
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-03-11 16:10:32 +0300

    half of code done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
+#define lmax 200
+
+using namespace std;
+
+int count_in_arr(int *arr, int len_arr, int el) {
+    int count = 0;
+    for (int *i = arr; i < arr + len_arr; i++) if (el == *i) count++;
+    return count;
+}
+
+void print_arr(int *arr, int len_arr) {
+    for (int *i = arr; i < arr + len_arr; i++) cout << *i << " ";
+    cout << "\n";
+}
+
+int function(int *A, int len_A, int *B, int len_B, int *C) {
+    int len_C = 0;
+    for (int *i = B; i < B + len_B; i++)
+        if (count_in_arr(B, len_B, *i) > 1 && !count_in_arr(A, len_A, *i))
+            *(C + len_C++) = *i;
+    return len_C;
+
+}
+
+
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    setlocale(LC_ALL,"Russian");
+    int A[lmax],B[lmax],C[lmax],len_A,len_B,len_C;
+
+
     return 0;
 }
